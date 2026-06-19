3c3e1613587a2cfd589a63d91024cf0fb5923b7e
Priyanshu Paul <Priyanshupaul32@gmail.com>
2026-06-20 01:17:33 +0530

    implemented persistence using inMemorySaver by the help of checkpoint and thread_id

--- Day 5/persistance.py
+++ Day 5/persistance.py
@@ -2,7 +2,7 @@ from langchain_groq import ChatGroq
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from dotenv import load_dotenv
-from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.memory import InMemorySaver
 
 load_dotenv()
 
@@ -37,5 +37,19 @@ graph.add_edge(START,"create_joke")
 graph.add_edge("create_joke","give_explanation")
 graph.add_edge("give_explanation",END)
 
+checkpointer = InMemorySaver()
+workflow = graph.compile(checkpointer=checkpointer)
 
-graph.compile
+config1 = {"configurable":{"thread_id":"1"}}
+config2 = {"configurable":{"thread_id":"2"}}
+result1 = workflow.invoke({"topic":"programming"}, config=config1)
+result2 = workflow.invoke({"topic":"football"}, config=config2)
+# print(result)
+
+answer = list(workflow.get_state(config=config2))
+# this will show all the checkpoints and there states and the final state will have the topic, joke and explanation all together this also show us the intermediate state as well as the enxt state and the checkpoint id 
+print(answer)
+
+history = list(workflow.get_state_history(config=config1))
+# this will show us the history of the states that we have gone through in this workflow with the checkpoint id and the state at that checkpoint id and the timestamp of when that checkpoint was created this
+print(history)
