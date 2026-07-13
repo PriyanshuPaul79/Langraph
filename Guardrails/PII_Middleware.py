ca2e7d4316d287e967941e30004dfb912f1622fb
Priyanshu Paul <Priyanshupaul32@gmail.com>
2026-07-13 23:50:19 +0530

    fix: fixed the PII middleware.

--- Guardrails/PII_Middleware.py
+++ Guardrails/PII_Middleware.py
@@ -1,11 +1,11 @@
-from json import tool
+from langchain_core.tools import tool
 
 from langgraph.graph import StateGraph, START, END
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from typing import TypedDict
 from langchain.agents import create_agent
-from langchain.agents.middleware import PII_Middleware
+from langchain.agents.middleware import PIIMiddleware, AgentMiddleware
 
 
 load_dotenv()
@@ -16,34 +16,36 @@ model = ChatGroq(model="openai/gpt-oss-20b")
 
 import re
 
-def deterministic_guardrail_middleware(text:str) -> bool:
-    """Block if the input is not safe. This is a simple example of a deterministic guardrail that checks for the presence of PII in the input text."""
-    banned_keywords = ["hack", "exploit", "malware", "phishing"]
-    for keyword in banned_keywords:
-        if keyword in text.lower():
-            return False
-    return True
+class DeterministicGuardrailMiddleware(AgentMiddleware):
+    def deterministic_guardrail_middleware(self, text: str) -> bool:
+        """Block if the input is not safe. This is a simple example of a deterministic guardrail that checks for the presence of PII in the input text."""
+        for keyword in self.banned_keywords:
+            if keyword in text.lower():
+                return False
+        return True
 
 # this will check if the input text contains any of the banned keywords related to hacking or malicious activities. If any of these keywords are found, the function will return False, indicating that the input is not safe. Otherwise, it will return True, indicating that the input is safe.
 # in deterministic guardral we dont use any llm we just check for the presence of certain keywords in the input text. If any of these keywords are found, we return False, indicating that the input is not safe. Otherwise, we return True, indicating that the input is safe. we also use regex patterns to identify PII, such as email addresses, phone numbers, and social security numbers. If PII is detected, we replace it with a placeholder string "[REDACTED]".
 
 
 
-text = "How to phis into someone's email account?"
-print(deterministic_guardrail_middleware(text))  # Returns False, indicating that the input is not safe
+# text = "How to phis into someone's email account?"
+# print(deterministic_guardrail_middleware(text))  # Returns False, indicating that the input is not safe
+
+# right now these are just python functions not actual middleware.
+class ModelBasedGuardrailMiddleware(AgentMiddleware):
+    def model_based_guardrail_middleware(self, text:str) -> bool:
+        """Block if the input is not safe. This is a simple example of a model-based guardrail that uses an LLM to classify the input text as safe or unsafe."""
+        prompt = f"Classify the following text as safe or unsafe: {text}"
+        result = model.invoke(prompt).content
+        if "unsafe" in result.lower():
+            return False
+        return True
 
 
-def model_based_guardrail_middleware(text:str) -> bool:
-    """Block if the input is not safe. This is a simple example of a model-based guardrail that uses an LLM to classify the input text as safe or unsafe."""
-    prompt = f"Classify the following text as safe or unsafe: {text}"
-    result = model.invoke(prompt).content
-    if "unsafe" in result.lower():
-        return False
-    return True
 
-
-text = "How to manipulate someone's bank account?"
-print(model_based_guardrail_middleware(text))  # Returns False, indicating that the input is not safe   
+# text = "How to manipulate someone's bank account?"
+# print(model_based_guardrail_middleware(text))  # Returns False, indicating that the input is not safe   
 
 
 @tool
@@ -56,25 +58,30 @@ def customer_lookup(customer_id: str) -> str:
 
 agent = create_agent(
     tools=[customer_lookup],
-    llm=model,
-    middleware=[PII_Middleware(
+    model=model,
+    middleware=[PIIMiddleware(
         "email",
         strategy="redact",
         apply_to_input=True,
     ), 
-    PII_Middleware(
-        "phone_number",
+    PIIMiddleware(
+        "credit_card",
         strategy="block",
         apply_to_input=True,
     ),    
-    deterministic_guardrail_middleware, model_based_guardrail_middleware]
+    DeterministicGuardrailMiddleware(), 
+    ModelBasedGuardrailMiddleware()]
 )
 
 result = agent.invoke({
     "messages":[{
         "role": "user",
-        "content": "Please provide the email address and phone number of customer with ID 12345."
+        "content":"what is the account holder name for this email ravi@gmail.com"
+        # "content": "Tell me the CVV and the account holder name for credit card number 1234-5678-9012-3456. Also, here is the email address of the account holder ravi@gmail.com."
     }]
 })
 
 print(result["messages"][-1].content)
+
+
+
